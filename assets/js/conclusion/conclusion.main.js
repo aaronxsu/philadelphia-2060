8a44a7b58ea5b4c244eaab23dd00c83ab85ff98c
Aaron Su <xunzesu@outlook.com>
2017-12-07 00:18:43 -0500

    Conclusion done for this round.

--- assets/js/conclusion/conclusion.main.js
+++ assets/js/conclusion/conclusion.main.js
@@ -1,3 +1,9 @@
+var changeSliderTopMargin = function() {
+    var h1 = $('.twentytwenty-wrapper.twentytwenty-horizontal').height();
+    var h2 = $('.twentytwenty-wrapper.twentytwenty-horizontal').parent().height();
+    $('.twentytwenty-wrapper.twentytwenty-horizontal').css('margin-top', (h2-h1)/2);
+}
+
 $(document).ready(function() {
   $('#climate-fullpage').fullpage({
       'lazyLoading': true,
@@ -5,4 +11,16 @@ $(document).ready(function() {
   });
 
   $('body').bootstrapMaterialDesign();
+
+  $('.side-by-side').twentytwenty({
+      no_overlay: true,
+      click_to_move: true,
+      default_offset_pct: 0.95
+  });
+
+  changeSliderTopMargin();
+
+  $(window).resize(function() {
+      changeSliderTopMargin();
+  });
 });
